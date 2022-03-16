03e3ae40e2018118e1089fb0fee2e487b21dbf62
Gozirin <chigozp2@gmail.com>
2022-03-16 09:04:41 +0100

    week5TaskPrecious

--- app/src/main/java/com/example/week5taskprecious/MainActivity.kt
+++ app/src/main/java/com/example/week5taskprecious/MainActivity.kt
@@ -1,27 +1,56 @@
 package com.example.week5taskprecious
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.Toast
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSendData: Button
+    private lateinit var editName: EditText
+    private lateinit var editPhone: EditText
+    private lateinit var editEmail: EditText
+    private lateinit var editGender: Spinner
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        Gender.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onNothingSelected(p0: AdapterView<*>?) {
-//
-//            }
-//
-//            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//             Toast.makeText(this@MainActivity,"You selected ${adapterView?.getItemAtPosition(position).toString()}",
-//                Toast.LENGTH_LONG).show()
-//            }
-//
-//        }
+        btnSendData = findViewById(R.id.button2)
+        editName = findViewById(R.id.editTextTextPersonName)
+        editPhone = findViewById(R.id.editTextTextPersonName2)
+        editEmail = findViewById(R.id.editTextTextPersonName4)
+        editGender = findViewById(R.id.Gender)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        btnSendData.setOnClickListener {
+
+            val intent = Intent(this, SecondActivity2::class.java).apply{
+                this.putExtra("Name",editName.text.toString())
+                this.putExtra("Phone",editPhone.text.toString())
+                this.putExtra("Email", editEmail.text.toString())
+                this.putExtra("Gender", editGender.toString())
+            }
+
+            startActivity(intent) // launch activity
+        }
+
+    }
+
 }
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/week5taskprecious/SecondActivity2.kt
@@ -0,0 +1,49 @@
+package com.example.week5taskprecious
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_second2.*
+import org.w3c.dom.Text
+import java.util.jar.Attributes
+
+class SecondActivity2 : AppCompatActivity() {
+
+
+
+
+
+    private lateinit var textName : TextView
+    private lateinit var textPhone : TextView
+    private  lateinit var textEmail : TextView
+    private lateinit var textGender : TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second2)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        textName = findViewById(R.id.textView2)
+        textPhone = findViewById(R.id.textView3)
+        textEmail = findViewById(R.id.textView5)
+        textGender = findViewById(R.id.textView6)
+
+        val Name = intent.getStringExtra( "Name")
+        val Phone = intent.getStringExtra( "Phone")
+        val Email = intent.getStringExtra( "Email")
+        val Gender = intent.getStringExtra( "Gender")
+
+
+
+        textName.text = Name
+        textPhone.text = Phone
+        textEmail.text = Email
+        textGender.text = Gender
+
+    }
+}
